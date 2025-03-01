1ee27e82d3c83d9d48c3542a1d8ee871666ac197
Den-Med <124715785+Den-Med@users.noreply.github.com>
2025-03-02 01:29:34 +0300

    Create Pikabu_hiddenLink.js

---
+++ Pikabu/Pikabu_hiddenLink.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Pikabu hiddenLink
+// @namespace    http://tampermonkey.net/
+// @version      2025-03-01
+// @description  try to take over the world!
+// @author       You
+// @match        https://pikabu.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pikabu.ru
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    // check CSS rule in "Pikabu style"
+    const textType = Node.TEXT_NODE
+    const linkList_tme = document.querySelectorAll('p > a[href^="https://t.me/"]');
+
+    linkList_tme.forEach((elemLink)=> {
+        let linkRange = [];
+        if (elemLink.previousSibling?.nodeType == textType) linkRange.push(elemLink.previousSibling);
+        linkRange.push(elemLink);
+        if (elemLink.nextSibling?.nodeType == textType) linkRange.push(elemLink.nextSibling);
+        const divLink = document.createElement('div');
+        divLink.className = 'hiddenLink';
+        elemLink.after(divLink);
+        divLink.append(...linkRange);
+    });
+
+})();
