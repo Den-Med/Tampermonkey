0cdef808fe014029f5a4662267354e59deefd99c
Den-Med <124715785+Den-Med@users.noreply.github.com>
2025-03-12 13:49:57 +0300

    Update Pikabu_hiddenLink.js

--- Pikabu/Pikabu_hiddenLink.js
+++ Pikabu/Pikabu_hiddenLink.js
@@ -19,18 +19,20 @@
 
         let eIndex = childArray.indexOf(elemLink);
 
-        let leftIndex = childArray.slice(0,eIndex).findLastIndex((e)=> e.nodeType == elemType );
+        let leftIndex = childArray.slice(0, eIndex).findLastIndex((e)=> e.nodeType == elemType );
         leftIndex = leftIndex >= 0 ? leftIndex + 1 : 0;
 
-        let rightIndex = childArray.slice(eIndex + 1, childArray.length + 1).findLastIndex((e)=> e.nodeType == elemType );
-        rightIndex = rightIndex >= 0 ? rightIndex - 1 : childArray.length
+        let rightIndex = childArray.slice(eIndex + 1).findIndex((e)=> e.nodeType == elemType );
+        rightIndex = rightIndex >= 0 ? rightIndex + eIndex : linkRange.length
 
         let linkRange = childArray.slice(leftIndex, rightIndex + 1);
+        if (linkRange.length == 0) return;
 
         const span = document.createElement('span');
-        span.className = 'hiddenLink';
+        span.className = hiddenCl;
         elemLink.after(span);
         span.append(...linkRange);
+        span.addEventListener('click', clickHandler);
     });
 
 })();
