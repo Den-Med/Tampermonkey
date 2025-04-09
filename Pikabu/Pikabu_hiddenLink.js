f24dee5dcc9737fc663d72a047215481880c6f69
Den-Med <124715785+Den-Med@users.noreply.github.com>
2025-04-09 10:23:31 +0300

    Update Pikabu_hiddenLink.js

--- Pikabu/Pikabu_hiddenLink.js
+++ Pikabu/Pikabu_hiddenLink.js
@@ -11,18 +11,34 @@
 
 (function() {
     // check CSS rule in "Pikabu style"
-    const elemType = Node.ELEMENT_NODE
-    const telegramLinks = document.querySelectorAll('p > a[href^="https://t.me/"]');
+    const obsAtt = {childList: true, subtree: true};
+    const hiddenCl = 'hiddenLink';
+    const notHiddenCl = 'notHiddenLink';
+    const targetLinks = 't.me, aliclick.shop, gbest.by, lres.bz'
 
-    telegramLinks.forEach((elemLink)=> {
+    const selector = targetLinks.split(',').map(item => `p > a[href*="${item.trim()}"]`).join(', ');
+
+    function catchListOfLinks(node) {
+        if (node.nodeType !== 1) return;
+        const nodeArray = [];
+        if (node.matches(selector)) nodeArray.push(node) ;
+        const links = node.querySelectorAll(selector);
+        nodeArray.push(...Array.from(links));
+        nodeArray.forEach(hideLink);
+    };
+
+    function findBR(e) {return e.nodeName === 'BR'};
+
+    function hideLink(elemLink) {
+        if (elemLink.parentNode.matches('.' + hiddenCl)) return;
+
         let childArray = Array.from(elemLink.parentNode.childNodes);
-
         let eIndex = childArray.indexOf(elemLink);
 
-        let leftIndex = childArray.slice(0, eIndex).findLastIndex((e)=> e.nodeType == elemType );
+        let leftIndex = childArray.slice(0, eIndex).findLastIndex(findBR);
         leftIndex = leftIndex >= 0 ? leftIndex + 1 : 0;
 
-        let rightIndex = childArray.slice(eIndex + 1).findIndex((e)=> e.nodeType == elemType );
+        let rightIndex = childArray.slice(eIndex + 1).findIndex(findBR);
         rightIndex = rightIndex >= 0 ? rightIndex + eIndex : childArray.length
 
         let linkRange = childArray.slice(leftIndex, rightIndex + 1);
@@ -32,7 +48,22 @@
         span.className = hiddenCl;
         elemLink.after(span);
         span.append(...linkRange);
-        span.addEventListener('click', clickHandler);
+        span.addEventListener('click', toggleClass);
+    };
+
+    function toggleClass(e){
+        const t = e.currentTarget.classList;
+        t.toggle(notHiddenCl, !t.toggle(hiddenCl))
+    };
+
+    catchListOfLinks(document.body);
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach(catchListOfLinks);
+      });
     });
+
+    observer.observe(document.body, obsAtt);
 
 })();
