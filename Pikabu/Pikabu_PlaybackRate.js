9e27ee8807b1950dfa1a6a5a298421578bdc8358
Den-Med <124715785+Den-Med@users.noreply.github.com>
2025-04-09 10:26:43 +0300

    Update Pikabu_PlaybackRate.js

--- Pikabu/Pikabu_PlaybackRate.js
+++ Pikabu/Pikabu_PlaybackRate.js
@@ -10,18 +10,38 @@
 // ==/UserScript==
 
 (function() {
-    let divPlaybackRate = document.createElement('div');
-    divPlaybackRate.className = 'player__playbackRate';
-    divPlaybackRate.style.cssText = 'cursor: pointer; padding: 16px 5px 16px 1px';
+    const obsAtt = {childList: true, subtree: true};
+    const w3svg = 'http://www.w3.org/2000/svg';
+    const stylePR = document.createElement('style');
+    const palyerConSel = 'div.player__controls-wrapper'
 
-    let svgElem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svgElem.style.cssText = 'width: 25px; height: 20px';
+    stylePR.textContent = `.player__playbackRate {cursor: pointer; padding: 16px 5px 16px 1px;}
+    .playbackValue {display: none; position: absolute; bottom: 50%; padding: 4px;}
+    .player__playbackRate:hover > .playbackValue {display: block;}`;
+    document.head.appendChild(stylePR);
 
-    let ellipseElem = document.createElementNS('http://www.w3.org/2000/svg', 'ellipse');
-    [['fill', '#0000'],['cx', 10],['cy', 10],['rx', 7],['ry', 7],['stroke-width', 3],['stroke', '#fff']].forEach(attr => ellipseElem.setAttribute(...attr));
 
-    svgElem.appendChild(ellipseElem);
-    divPlaybackRate.appendChild(svgElem);
+    function createDivPR(){
+        const divPlaybackRate = document.createElement('div');
+        divPlaybackRate.className = 'player__playbackRate';
+        //divPlaybackRate.style.cssText = 'cursor: pointer; padding: 16px 5px 16px 1px';
+
+        const divPRVal = document.createElement('div');
+        divPRVal.className = 'playbackValue'
+        divPRVal.textContent = '1.0'
+
+        const svgElem = document.createElementNS(w3svg, 'svg');
+        svgElem.style.cssText = 'width: 25px; height: 20px';
+
+        const ellipseElem = document.createElementNS(w3svg, 'circle');
+        [['fill', '#0000'],['cx', 10],['cy', 10],['r', 7],['stroke-width', 3],['stroke', '#fff']].forEach(attr => ellipseElem.setAttribute(...attr));
+
+        svgElem.appendChild(ellipseElem);
+        divPlaybackRate.appendChild(svgElem);
+        divPlaybackRate.appendChild(divPRVal);
+
+        return divPlaybackRate;
+    }
 
     function handleWheelPR(event) {
       event.preventDefault(); // Предотвращаем скролл страницы
@@ -29,33 +49,35 @@
       const video = event.target.closest('.player__player').querySelector('video');
       video.playbackRate += playbackRateDelta;
       video.playbackRate = Math.min(Math.max(video.playbackRate, 0.1), 3); // Ограничиваем диапазон скорости (0.1 - 3)
+      const divPRVal = event.target.querySelector('.playbackValue');
+      divPRVal.textContent = video.playbackRate.toFixed(1);
     };
 
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        mutation.addedNodes.forEach((node) => {
-          if (node.nodeType === Node.ELEMENT_NODE) {
-            if (node.matches('div.player__controls-wrapper')) {
-                prepareHandlerWPR(node);
-            }
-
-            const nestedElements = node.querySelectorAll('div.player__controls-wrapper');
-            nestedElements.forEach((element) => {
-                prepareHandlerWPR(element);
-            });
-          }
-        });
-      });
-    });
-
-    observer.observe(document.documentElement, {
-      childList: true,
-      subtree: true
-    });
-
     function prepareHandlerWPR(element) {
-        const divPR = divPlaybackRate.cloneNode(true)
+        const divPR = createDivPR(); //divPlaybackRate.cloneNode(true)
         element.lastElementChild.appendChild(divPR);
         divPR.addEventListener('wheel', handleWheelPR);
     }
+
+    function catchHandlerNodes(node) {
+        if (node.nodeType !== 1) return;
+        let nodeArray = [];
+        if (node.matches(palyerConSel)) nodeArray.push(node) ;
+        const nestedElements = node.querySelectorAll(palyerConSel);
+        nodeArray.push(...Array.from(nestedElements));
+        nodeArray.forEach(prepareHandlerWPR);
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach(catchHandlerNodes);
+      });
+    });
+
+    observer.observe(document.body, obsAtt);
+//     document.querySelectorAll('.player').forEach((e)=>{
+//         observer.observe(e, obsAtt);
+//     });
+
+
 })();
